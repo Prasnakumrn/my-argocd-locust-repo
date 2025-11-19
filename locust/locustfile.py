1b2bbc59fa9fe156772bccce3d2248f006d0bcaf
Prasnakumrn <prasannakumaran.dhanasekaran@commure.com>
2025-11-19 15:42:17 -0800

    adding locust

---
+++ locust/locustfile.py
@@ -0,0 +1,9 @@
+# locust/locustfile.py
+from locust import HttpUser, task, between
+
+class QuickUser(HttpUser):
+    wait_time = between(1, 2)
+
+    @task
+    def index(self):
+        self.client.get("/")
